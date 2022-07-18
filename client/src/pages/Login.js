07e190b357618466b40cd02a7214dd79fec27797
onwaxcomm@gmail.com <onwaxcomm@gmail.com>
2022-07-18 21:20:45 +0200

    adding the forms in the login container

---
+++ client/src/pages/Login.js
@@ -0,0 +1,19 @@
+//import {} for './../components/Styles';
+import { Formik, Form } from 'formik;'
+
+const Login = () => {
+  return (
+    <div>
+       <h2>Please LogIn</h2>
+       <Formik>
+          {() = > (
+            <Form>
+
+            </Form>
+          )}
+       </Formik>
+    </div>
+  )
+};
+
+export default Login;
