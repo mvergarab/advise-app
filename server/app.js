7e76d4f0cd97f90700e58422dda2d33210e80c91
onwaxcomm@gmail.com <onwaxcomm@gmail.com>
2022-07-25 15:47:51 +0200

    adding the database

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
-const express = require("express");
-const mysql = require("mysql2");
-
+const express = require('express');
 const app = express();
+const mysql = require('mysql2');
+const cors = require('cors');
+
+// https://stackoverflow.com/questions/50093144/mysql-8-0-client-does-not-support-authentication-protocol-requested-by-server
+
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
-
-
+  user: 'root',
+  host: 'localhost',
+  password: 'Lamaracuja123',
+  database: 'advise'
 });
+
+
+
+
 
 app.listen(3001, () => {
   console.log("running server");
